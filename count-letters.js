97ea6dfddedbc89b84f56e9a472e7d6622b055cd
S-Marks <marks.cherie.art@gmail.com>
2019-05-29 21:53:12 +0000

    Can count stuff

--- count-letters.js
+++ count-letters.js
@@ -1,7 +1,21 @@
 var str = process.argv.slice(2).join("").split("");
-//
-// function countLetters(str) {
-//   for(var i in str)
-// }
 
-console.log(str)
+function countLetters(str) {
+  var letters = {};
+  for(var i in str) {
+    var char = str[i];
+    if(letters[char]) {
+      letters[char] += 1
+    } else {
+      letters[char] = 1;  
+    }
+
+    // if(str[i] !=) {
+    //   str[i] += 1
+    // } else if
+  }
+
+  console.log(letters);
+};
+
+countLetters(str);
